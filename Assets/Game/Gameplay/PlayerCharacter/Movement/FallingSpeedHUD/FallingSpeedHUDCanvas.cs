002c901351145c9a94b45e82f70972e87ddac303
Fergal MECHIN <mechin.fergal@gmail.com>
2025-11-15 23:59:57 +0100

    Basic WIP Falling Speed HUD

---
+++ Assets/Game/Gameplay/PlayerCharacter/Movement/FallingSpeedHUD/FallingSpeedHUDCanvas.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using Game.Gameplay.Controls;
+using TMPro;
+using UnityEngine;
+
+namespace Game.Gameplay.PlayerCharacter.Movement.FallingSpeedHUD
+{
+    public class FallingSpeedHUDCanvas : MonoBehaviour
+    {
+        [SerializeField]
+        private float m_maxFallingSpeed = 5f;
+
+        [SerializeField]
+        private GameObject m_container;
+        
+        [SerializeField]
+        private TMP_Text m_fallingSpeedText;
+
+        [SerializeField]
+        private RectTransform m_fallingSpeedFiller;
+
+        private PlayerCharacter m_localCharacter;
+        
+        IEnumerator Start()
+        {
+            var localPlayerController = LocalPlayerController.Instance;
+            yield return new WaitUntil(() => localPlayerController.AssignedCharacter != null);
+            localPlayerController.OnCharacterAssigned += HandleNewCharacterAssigned;
+            HandleNewCharacterAssigned(localPlayerController);
+        }
+
+        private void HandleNewCharacterAssigned(LocalPlayerController a_localPlayerController)
+        {
+            m_localCharacter = a_localPlayerController.AssignedCharacter;
+        }
+
+        private void Update()
+        {
+            if (!m_localCharacter)
+                return;
+
+            if (m_localCharacter.IsGroundedController.IsGrounded)
+            {
+                m_container.SetActive(false);
+                return;
+            }
+            m_container.SetActive(true);
+            
+            var verticalSpeed = m_localCharacter.MovementController.VerticalSpeed;
+            
+            m_fallingSpeedText.text = $"{verticalSpeed:0} m/s";
+            var anchorTemp = m_fallingSpeedFiller.anchorMax;
+            anchorTemp.x =  Mathf.Clamp01(-verticalSpeed / m_maxFallingSpeed);
+            m_fallingSpeedFiller.anchorMax = anchorTemp;
+        }
+    }
+}
